faca0ae19e13427b2807a35c2dd948d00464de66
kylesnowschwartz <kyle.snowschwartz@gmail.com>
2016-02-11 16:45:13 +1300

    polling working and the option to play the AI or to play a human

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-  var selected_squares = []
+  var selected_squares = [];
+  var initialTurnCount = $(".chessboard").data("turn-count");
 
   $(".chessboard").click(function(event) {
-    $(event.target).addClass('selected')
+    $(event.target).addClass('selected');
 
     selected_squares.push($(event.target))
 
     if (selected_squares.length === 2) {
-      var from = selected_squares[0].attr('id')
-      var to = selected_squares[1].attr('id')
+      var from = selected_squares[0].attr('id');
+      var to = selected_squares[1].attr('id');
 
       if (from === to) {
         selected_squares.length = 0
@@ -25,13 +26,53 @@ $(document).ready(function() {
           error:function() {
            console.log('Error')
           }
-        })
+        });
         // TODO - refactor to use promises
 
         selected_squares = []
 
         window.location.reload()
+      };
+    };
+  });
+
+  function poll(fn, callback, timeout, interval) {
+    var endTime = Number(new Date()) + (timeout || 2000);
+    interval = interval || 100;
+
+    (function p() {
+      // If the condition is met, callback
+      if(fn()) {
+        callback();
       }
-    }
-  })
+      // If the condition isn't met but the timeout hasn't elapsed, go again
+      else if (Number(new Date()) < endTime) {
+        setTimeout(p, interval);
+      }
+    })();
+  }
+
+  if ($(".chessboard").length !== 0) {
+    poll(
+      function() {
+        jQuery.ajax({
+          url: location.href,
+          success: function (result) {
+            newTurnCount = parseInt($(result).find(".chessboard").attr("data-turn-count"))
+          },
+          async: false
+        });
+
+        if (initialTurnCount !== newTurnCount) {
+          return true
+        };
+      },
+      function() {
+        window.location.reload()
+      },
+      300000,
+      1000
+    );
+  }
+
 })
--- app/services/ai_move.rb
+++ app/services/ai_move.rb
@@ -7,27 +7,48 @@ class AIMove
   end
 
   def call
-    black_pieces_moves = black_pieces.map do |piece|
-      validator_for_piece(piece)
-    end.select { |piece_info| piece_info[:moves].any?}
+    black_potential_moves = pieces_potential_moves(black_pieces)
+  
+    p random = pieces_validated_moves(black_potential_moves).sample
+    random_move = random[:moves].select { |move| @board.position(move).present? }.sample || random[:moves].sample
 
-    p random = black_pieces_moves.sample
-
-    @new_move = Move.new(@board, random[:piece], @board.position(random[:piece]), random[:moves].sample)
-
-
-    @game.turns.create!(to_square: @new_move.to, from_square: @new_move.from)
+    if random
+      @game.turns.create!(to_square: random_move, from_square: @board.position(random[:piece]))
+    else
+      p "!!!! CHECKMATE !!!!"
+    end
   end
 
   def black_pieces
     @board.current_positions.select { |pieces| pieces.black? }
   end
 
-  def validator_for_piece(piece)
+  def potential_moves(piece)
     dummy_move = Move.new(@board, piece, @board.position(piece))
 
     validator = ValidatePieceMove.validator_for(dummy_move)
 
-    {piece: piece, moves: validator.potential_moves}
+    { piece: piece, moves: validator.potential_moves }
+  end
+
+  def pieces_potential_moves(pieces)
+    pieces
+      .map { |piece| potential_moves(piece) }
+      .select { |piece_info| piece_info[:moves].any? }
+  end
+
+  def pieces_validated_moves(pieces_info)
+    pieces_info.map do |piece_info|
+      piece = piece_info[:piece]
+      moves = piece_info[:moves]
+
+      validated_moves = moves.select do |move| 
+        current_move = Move.new(@board, piece, @board.position(piece), move)
+
+        ValidatePieceMove.validator_for(current_move).call
+      end
+
+      {piece: piece, moves: validated_moves}
+    end.select { |piece_info| piece_info[:moves].any?}
   end
 end
